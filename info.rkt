dbff260b6d300e3a75bce5322e97bad5be529e3d
Greg Hendershott <greghendershott@gmail.com>
2013-04-11 11:23:31 -0400

    Change info.rkt to one used for pkg mgr

--- info.rkt
+++ info.rkt
@@ -1,19 +1,2 @@
 #lang setup/infotab
-
-(define name "http")
-(define categories '(net))
-(define blurb
-  '("Provides support for HTTP 1.0 and 1.1 connections as well as for headers used in HTTP requests and responses."))
-(define homepage "https://github.com/greghendershott/http")
-
-(define release-notes
-  '((p "Use Racket's new define-logger.")
-    (p "Add a 'basic style to seconds->gmt-8601-string.")))
-(define version "2013-04-11")
-(define can-be-loaded-with 'all)
-
-(define primary-file '("main.rkt"))
-(define scribblings '(("manual.scrbl" (multi-page))))
-
-(define required-core-version "5.3.1.9")
-(define repositories '("4.x"))
+(define deps '())
