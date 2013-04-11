7c033f588670c88eb7d46bfbf88d3ef1eb346f41
Greg Hendershott <greghendershott@gmail.com>
2013-04-11 08:51:39 -0400

    Update info.rkt for v1.3

--- info.rkt
+++ info.rkt
@@ -3,17 +3,17 @@
 (define name "http")
 (define categories '(net))
 (define blurb
-  '("Provides support for HTTP 1.0 and 1.1 connections,"
-    "as well as for headers used in HTTP requests and responses."))
+  '("Provides support for HTTP 1.0 and 1.1 connections as well as for headers used in HTTP requests and responses."))
 (define homepage "https://github.com/greghendershott/http")
 
 (define release-notes
-  '((p "Fix formatting of code examples.")))
-(define version "2012-08-18")
+  '((p "Use Racket's new define-logger.")
+    (p "Add a 'basic style to seconds->gmt-8601-string.")))
+(define version "2013-04-11")
 (define can-be-loaded-with 'all)
 
 (define primary-file '("main.rkt"))
 (define scribblings '(("manual.scrbl" (multi-page))))
 
-(define required-core-version "5.3.1")
+(define required-core-version "5.3.1.9")
 (define repositories '("4.x"))
